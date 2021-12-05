4530fb6620da75e28e0d6182394a069aaa13340c
arunsaraswat123 <saraswatarun123@gmail.com>
2021-12-05 21:16:37 +0530

    Added account.js to maintain account history

--- react-app-example/src/index.js
+++ react-app-example/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Account from './Account';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Account></Account>
   </React.StrictMode>,
   document.getElementById('root')
 );
